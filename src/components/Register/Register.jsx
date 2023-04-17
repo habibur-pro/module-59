106b6a4bb152a068669f1e427424aaf664e6bc4d
habibur <va.habibur@gmail.com>
2023-04-17 11:48:08 +0600

    complete 59.2

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import './Register.css'
 
 const Register = () => {
+
+    const handelFormSubmit = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password)
+    }
+
     return (
         <div>
             <h3>Please Register</h3>
 
-            <form action="" style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+            <form action="" onSubmit={handelFormSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
                 <input
                     type="email"
                     placeholder='Your Email'
+                    name='email'
 
                 />
                 <input
                     type="password"
                     placeholder='Your Password'
+                    name='password'
 
                 />
                 <input
